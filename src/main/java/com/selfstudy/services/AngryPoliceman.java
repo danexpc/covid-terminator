eea64ac36f3521cd52ef079103a3c8f8c6dff486
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-20 18:08:24 +0300

    Create AngryPoliceman: implementation of Policeman interface

---
+++ src/main/java/com/selfstudy/services/AngryPoliceman.java
@@ -0,0 +1,8 @@
+package com.selfstudy.services;
+
+public class AngryPoliceman implements Policeman {
+    @Override
+    public void makePeopleLeave() {
+        System.out.println("I am going to kill all of you!");
+    }
+}
